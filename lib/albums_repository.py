f7357669094651a758f9c1b7ed9f3525042238a6
Sam Murgatroyd <sammurgatroyd30@gmail.com>
2023-10-05 16:40:44 +0100

    test drive cre and del commit

--- lib/albums_repository.py
+++ lib/albums_repository.py
@@ -15,8 +15,13 @@ class AlbumRepository:
     #get one album from database dependent on id
 
     def find(self, id):
-        rows = self._connection.execute(f'SELECT * FROM albums WHERE id = {id}')
-        for row in rows:
-            item = Albums(row['title'], row['release_year'], row['artist_id'])
+        rows = self._connection.execute(f'SELECT * FROM albums WHERE id = %s', [id])
+        row = rows[0]
+        item = Albums(row['title'], row['release_year'], row['artist_id'])
         return item
-
+    def create(self, new_record):
+        self._connection.execute(f'INSERT INTO albums (title, release_year, artist_id) VALUES (%s, %s, %s)', [new_record.title, new_record.release_year, new_record.artist_id])
+        return None
+    def delete(self, id):
+        self._connection.execute(f'DELETE FROM albums WHERE id = %s', [id])
+        return None
--- tests/test_albums_repository.py
+++ tests/test_albums_repository.py
@@ -29,3 +29,43 @@ def test_if_find_method_returns_single_album_object(db_connection):
     db_connection.seed('seeds/music_library.sql')
     repository = AlbumRepository(db_connection)
     assert repository.find(1) == Albums('Doolittle', 1989, 1)
+
+def test_create_returns_all_with_additional_record(db_connection):
+    db_connection.seed('seeds/music_library.sql')
+    repository = AlbumRepository(db_connection)
+    repository.create(Albums('Money money money', 1976, 2))
+    result = repository.all()
+    assert result == [
+        Albums('Doolittle', 1989, 1),
+        Albums('Surfer Rosa', 1988, 1),
+        Albums('Waterloo', 1974, 2),
+        Albums('Super Trouper', 1980, 2),
+        Albums('Bossanova', 1990, 1),
+        Albums('Lover', 2019, 3),
+        Albums('Folklore', 2020, 3),
+        Albums('I Put a Spell on You', 1965, 4),
+        Albums('Baltimore', 1978, 4),
+        Albums('Here Comes the Sun', 1971, 4),
+        Albums('Fodder on My Wings', 1982, 4),
+        Albums('Ring Ring', 1973, 2),
+        Albums('Money money money', 1976, 2)
+        ]
+    
+def test_create_returns_all_with_additional_record(db_connection):
+    db_connection.seed('seeds/music_library.sql')
+    repository = AlbumRepository(db_connection)
+    repository.delete(3)
+    result = repository.all()
+    assert result == [
+        Albums('Doolittle', 1989, 1),
+        Albums('Surfer Rosa', 1988, 1),
+        Albums('Super Trouper', 1980, 2),
+        Albums('Bossanova', 1990, 1),
+        Albums('Lover', 2019, 3),
+        Albums('Folklore', 2020, 3),
+        Albums('I Put a Spell on You', 1965, 4),
+        Albums('Baltimore', 1978, 4),
+        Albums('Here Comes the Sun', 1971, 4),
+        Albums('Fodder on My Wings', 1982, 4),
+        Albums('Ring Ring', 1973, 2),
+        ]
